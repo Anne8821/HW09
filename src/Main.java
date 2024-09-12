597306d6d6836bd2733ffcd3d5d4110763543061
Anne8821 <makurinaa@list.ru>
2024-09-13 00:38:15 +0300

    hw9

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,48 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        System.out.println("Задача 1");
+        int totalSalary = 0;
+        int [] salaryArray = {123, 456, 789, 765, 543};
+        for (int salary : salaryArray){
+            totalSalary += salary;
+        }
+        System.out.println("Сумма трат за месяц составила " + totalSalary + " рублей");
+        System.out.print("");
+
+        System.out.println("Задача 2");
+        int [] arr = {123, 456, 789, 765, 543};
+        int min = arr [0];
+        int max = arr [0];
+
+        for (int element : arr){
+            if (element < min){
+                min = element;
+            }else if (element > max){
+                max = element;
+            }
+        }
+        System.out.println("Минимальная сумма трат за неделю составила " + min + " рублей.");
+        System.out.println("Максимальная сумма трат за неделю составила " + max + " рублей");
+        System.out.print("");
+
+        System.out.println("Задача 3");
+        int totalMoney = 0;
+        int [] moneyArray = {123, 456, 789, 765, 543};
+        for (int money : moneyArray){
+            totalMoney += money;
+        }
+        System.out.println("Средняя сумма трат за месяц составила " + totalMoney / moneyArray.length + " рублей");
+        System.out.print("");
+
+        System.out.println("Задача 4");
+        char [] reverseFullName = {'n', 'a', 'v', 'I', ' ', 'v', 'o', 'n', 'a', 'v', 'I'};
+        int lenght = reverseFullName.length;
+        for ( int i = 0; i < lenght / 2; i++){
+            char cash = reverseFullName[i];
+            reverseFullName[i] = reverseFullName[lenght -1 - i];
+            reverseFullName[lenght - 1 - i] = cash;
+        }
+        System.out.println(reverseFullName);
     }
 }
